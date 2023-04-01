1e5052222d4284bdfdac677e553d8692d02cd3e7
Harry Bird <ygoduelistharry@gmail.com>
2023-04-02 01:10:24 +0800

    Removed pandas dependency and cleaned up venv

--- seven_wonders_duel.py
+++ seven_wonders_duel.py
@@ -1,32 +1,100 @@
 '''Module to play a game of Seven Wonders Duel'''
 import csv
-import pandas as pd
+from ast import literal_eval as leval
 from numpy.random import default_rng
 from sty import fg, bg, rs
 
 
+class Card:
+    '''Define a single card. Attributes match the .csv headers'''
+    colour_key = {
+        "Brown": bg(100, 50, 0) + fg.white,
+        "Grey": bg.grey + fg.black,
+        "Red": bg.red + fg.white,
+        "Green": bg(0, 128, 0) + fg.white,
+        "Yellow": bg.yellow + fg.black,
+        "Blue": bg.blue + fg.white,
+        "Purple": bg(128, 0, 128) + fg.white,
+    }
+
+    def __init__(self, card_name='', card_set='', card_type='', card_cost='', card_age='',
+        card_effect_passive='', card_effect_when_played='', card_prerequisite=''):
+        self.card_name = card_name
+        self.card_set = card_set
+        self.card_type = card_type
+        self.card_cost = card_cost
+        self.card_effect_passive = card_effect_passive
+        self.card_effect_when_played = card_effect_when_played
+        self.card_age = card_age
+        self.card_prerequisite = card_prerequisite
+
+    def __repr__(self):
+        return str(Card.colour_key[self.card_type]
+                   + self.card_name
+                   + rs.all)
+
+
+def csv_to_class(csv_file:str, to_class, string=False):
+    """Converts a .csv to a list of class objects of type to_class.  Class must take only kwargs as variables.
+    The .csv headers must match the kwargs of the to_class.
+
+    Args:
+        csv_file (str): .csv filename.
+        to_class (_type_): Class objects to be generated.
+        string (bool, optional): Set string=True to retain kwargs as strings. Defaults to False.
+        Uses ast.literal_eval() to convert all kwargs to non-string types. 
+
+    Returns:
+        (list): list of generated 'to_class' objects
+    """
+
+    with open(csv_file, encoding='UTF-8') as file:
+        kwarg_dict_list = list(csv.DictReader(file))
+    file.close()
+
+    if string is False:
+        for kwarg_dict in kwarg_dict_list:
+            for key, value in kwarg_dict.items():
+                try:
+                    kwarg_dict[key] = leval(value)
+                except SyntaxError:
+                    pass
+                except ValueError:
+                    pass
+
+    list_of_class_objects = [to_class(**kwargs) for kwargs in kwarg_dict_list]
+
+    return list_of_class_objects
+
+
 class Game:
     '''Define a single instance of a game'''
+    # TODO need to track discard pile as some wonders revive cards
+    all_cards = csv_to_class('card_list.csv',Card)
 
-    def __init__(self, game_count=1):
+    def __init__(self, game_id=1, active=0):
         # Create a list of lists, one list per age containing the card objects for that age:
-        self.age_boards = [Age(age) for age in range(1, 4)]
-        self.game_count = game_count
+        self.age_boards = {'1':Age(1)}
+        self.game_id = game_id
+        self.active = active
         self.players = [Player(0, 'human'), Player(1, 'human')]
         self.state_variables = StateVariables()
         self.display_game_state()
 
     def __repr__(self):
-        return repr('Game Instance: ' + str(self.game_count))
+        return repr('Game Instance: ' + str(self.game_id))
 
         # TODO: Draft wonders function
 
-    def request_player_input(self):  # TODO When using AI, no need for player input, just needs to print AI choice.
+    def request_player_input(self):  # TODO When using AI, no need for player input.
         """Function to begin requesting player input
 
         Returns:
             void: [description]
         """
+        if self.active == 0:
+            return
+
         choice = input("PLAYER " + str(self.state_variables.turn_player + 1) + ": "
                        + "Select a card to [c]onstruct or [d]iscard for coins. "
                        + "(Format is 'X#' where X is c/d and # is card position)")  # TODO Select by name or number?
@@ -35,8 +103,8 @@ class Game:
         if action == 'q':
             return print("Game has been quit")
 
-        if action != 'c' and action != 'd':
-            print("Select a valid action! (construct or discard)")
+        if action not in ['c','d']:
+            print("Select a valid action! ([c]onstruct or [d]iscard)")
             return self.request_player_input()
 
         if not position.isdigit():
@@ -60,7 +128,7 @@ class Game:
 
         # Current age variables
         age = self.state_variables.current_age
-        slots_in_age = self.age_boards[age].card_positions
+        slots_in_age = self.age_boards[str(age)].card_positions
 
         # Checks for valid card choices
         if position >= len(slots_in_age) or position < 0:
@@ -80,7 +148,7 @@ class Game:
         # Discard or construct chosen card and remove card from board
         match action:
             case 'c':  # Add card to board.
-                if self.card_constructable(player_state, chosen_position.card_in_slot) is True:
+                if card_constructable(player_state, opponent_state, chosen_position.card_in_slot) is True:
                     player_state.construct_card(chosen_position.card_in_slot)
                 else:
                     print('You do not have the resources required to construct this card!')
@@ -95,31 +163,30 @@ class Game:
         chosen_position.card_in_slot = None
         player_state.update()
 
-        # Check for end of age (all cards drafted)
-        if all(slots_in_age[slot].card_in_slot is None for slot in range(len(slots_in_age))):
-            self.state_variables.progress_age()
-        else:  # Otherwise, update all cards in current age and change turn turn_player
-            self.age_boards[age].update_all()
-            self.state_variables.change_turn_player()  # TODO This might not always be true if go again wonders chosen
-
-        if self.state_variables.game_end:
-            self.display_game_state()
-            return print('Game is over!')  # TODO Check victory and stuff
+        # Check for end of age
+        self.check_age_end()
 
         # Continue game loop.
         self.display_game_state()
         return self.request_player_input()
 
-    # Takes 2 Player objects and 1 Card object and checks whether card is constructable given state and cost.
-    # TODO Check whether card is constructable given arbitrary player/opponent/card objects
-    def card_constructable(self, player, card):
-        '''Checks whether a card is constructable given current player states'''
-        return True
+    def check_age_end(self):
+        '''Checks if there are cards left in the age, and if not, progresses to the next age'''
 
-    # Takes 2 Player objects and 1 Card object and constructs the card if possible. If it cannot, returns False.
-    def valid_moves(self, player, opponent, age):  # TODO Return list of valid moves for current player.
-        '''Returns list of valid moves for given board state and player states'''
-        return
+        # Current age variables
+        age = self.state_variables.current_age
+        slots_in_age = self.age_boards[str(age)].card_positions
+
+        if all(slots_in_age[slot].card_in_slot is None for slot in range(len(slots_in_age))):
+            self.state_variables.progress_age()
+
+        else:  # Otherwise, update all cards in current age and change turn turn_player
+            self.age_boards[str(age)].update_all_slots()
+            self.state_variables.change_turn_player()  # TODO This might not always be true if go again wonders chosen
+
+        if self.state_variables.game_end:
+            self.display_game_state()
+            return print('Game is over!')  # TODO Check victory and stuff
 
     # Displays the game state in a nice enough way.
     def display_game_state(self):
@@ -127,39 +194,10 @@ class Game:
         player = self.state_variables.turn_player
         age = self.state_variables.current_age
 
-        self.age_boards[age].display_board()
+        self.age_boards[str(age)].display_board()
         print("Player 1 >", self.players[0])
         print("Player 2 >", self.players[1])
         print("Current turn player is Player ", str(player + 1))
-
-
-class Card:
-    '''Define a single card. Attributes match the .csv headers'''
-    colour_key = {
-        "Brown": bg(100, 50, 0) + fg.white,
-        "Grey": bg.grey + fg.black,
-        "Red": bg.red + fg.white,
-        "Green": bg(0, 128, 0) + fg.white,
-        "Yellow": bg.yellow + fg.black,
-        "Blue": bg.blue + fg.white,
-        "Purple": bg(128, 0, 128) + fg.white,
-    }
-
-    def __init__(self, card_name=0, card_set=0, card_type=0, card_cost=0, card_age=0, card_effect_passive=0,
-                 card_effect_when_played=0, card_prerequisite=0):
-        self.card_name = card_name
-        self.card_set = card_set
-        self.card_type = card_type
-        self.card_cost = card_cost
-        self.card_effect_passive = card_effect_passive
-        self.card_effect_when_played = card_effect_when_played
-        self.card_age = card_age
-        self.card_prerequisite = card_prerequisite
-
-    def __repr__(self):
-        return str(self.colour_key[self.card_type]
-                   + self.card_name
-                   + rs.all)
 
 
 class CardSlot:
@@ -172,13 +210,13 @@ class CardSlot:
         self.card_in_slot = card_in_slot
         self.card_visible = card_visible
         self.card_selectable = card_selectable
-        if isinstance(covered_by, str):
+        if covered_by:
             self.covered_by = [int(card) for card in str(covered_by).split(" ")]
         else:
             self.covered_by = []
         self.row = row
 
-    def __repr__(self):  # How the cards are displayed to the players.
+    def __repr__(self):  # How the cards in structure are displayed to the players.
         if self.card_in_slot is None:
             return str("")
 
@@ -224,7 +262,6 @@ class Player:
     def construct_card(self, card):
         '''Fucntion to construct a card in a players tableau'''
         self.cards_in_play.append(card)
-        return
 
     def update(self):
         '''Updates player passive variables based on players tableau'''
@@ -234,10 +271,11 @@ class Player:
 class StateVariables:
     '''Class to represent all state variables shared between players (military, turn player, etc.)'''
 
-    def __init__(self, turn_player=None, current_age=0, military_track=0):
+    def __init__(self, turn_player=None, current_age=1, military_track=0):
         self.rng = default_rng()
         if turn_player is None:
-            self.turn_player = self.rng.integers(low=0, high=2)  # Randomly select first player if none specified.
+            self.turn_player = self.rng.integers(low=0, high=1, endpoint=True)
+            # Randomly select first player if none specified.
         self.current_age = current_age  # Start in first age.
         self.military_track = military_track  # Start military track at 0.
         self.game_end = False
@@ -249,7 +287,7 @@ class StateVariables:
     def progress_age(self):
         '''Function to progress age and end game if required'''
         # TODO For progress age function: check military track for turn player and deal with end of game.
-        if self.current_age < 2:
+        if self.current_age < 3:
             self.current_age = self.current_age + 1
         else:
             self.game_end = True
@@ -257,30 +295,32 @@ class StateVariables:
 
 class Age:
     '''Class to define a game age and represent the unique board layouts'''
-    # TODO !!! Remove dependency on pandas for csv load and board setup
-    #
-    # Import card layout for each age:
-    age_layouts = pd.read_csv('age_layout.csv', dtype={
-        'game_age': 'category',
-    })
 
+    all_card_slots = csv_to_class('age_layout.csv', CardSlot)
 
-    # Import full card list:
-    card_list = pd.read_csv('card_list.csv', dtype={
-        'card_set': 'category',  # Set as category to speed up filter
-        'card_type': 'category',
-        'card_age': 'category',
-    })
+    ages = [1,2,3]
+    age_card_counts = {
+        '1':{'1':20},
+        '2':{'2':20},
+        '3':{'3':17,'Guild':3}
+    }
+    age_layouts = {}
 
-    age_1_card_count = 20
-    age_2_card_count = 20
-    age_3_card_count = 17
-    age_guild_card_count = 3
+    for age in ages:
+        age_layout = []
+        for slot in all_card_slots:
+            if slot.game_age == age:
+                age_layout.append(slot)
+        age_layouts[str(age)] = age_layout
+
+    # Generates a dict of lists holding initial card slot details for each age.
+    # Couldn't use nested dict/list comprehension because inner comprehesions cant access outer scope to iterate over,
+    # namely all_card_slots.
 
     def __init__(self, age):
+        self.rng = default_rng()
         self.age = age
         self.card_positions = self.prepare_age_board(age)
-        self.number_of_rows = max(self.card_positions[slot].row for slot in range(len(self.card_positions)))
 
     def __repr__(self):
         return str('Age ' + str(self.age))
@@ -288,41 +328,29 @@ class Age:
     # Init functions:
 
     def prepare_age_board(self, age):
-        '''Takes dataframe of all cards and creates list of card objects representing the board for a given age.'''
-        age = str(age)  # Convert to string if required
-        age_layout = self.age_layouts.loc[self.age_layouts['game_age'] == age]. \
-            reset_index(drop=True)  # Filter for age & reset index
-        age_cards = self.card_list.loc[self.card_list['card_age'] == age]  # Filter for age
+        '''Generates a game board for a specified age'''
+        # Randomly select cards from card pool(s) based on age, card type, and quantity specified in age_card_counts.
+        age_cards = [] # Will be a list of card objects selected randomly from all_cards.
 
-        match age:
-            case '1':  # Select random cards for age 1
-                age_cards = age_cards.sample(self.age_1_card_count).reset_index(drop=True)
-            case '2':  # Select random cards for age 2
-                age_cards = age_cards.sample(self.age_2_card_count).reset_index(drop=True)
-            case '3':  # Select guild cards and cards for age 3
-                guilds_chosen = self.card_list.loc[self.card_list['card_age'] == 'Guild'].sample(
-                    self.age_guild_card_count)
-                age_cards = age_cards.sample(self.age_3_card_count)
-                age_cards = age_cards.append(guilds_chosen)  # Add guild cards and normal cards together
-                age_cards = age_cards.sample(frac=1).reset_index(drop=True)  # Shuffle cards together
-            case _:
-                return
+        for card_type, count in self.age_card_counts[str(age)].items():
+            card_pool = [card for card in Game.all_cards if str(card.card_age)==card_type]
+            chosen_cards = list(self.rng.choice(card_pool, size=count, replace=False))
+            age_cards.extend(chosen_cards)
 
-        # Unpack age layout dataframe in card slot objects
-        card_positions = [CardSlot(**value)
-                          for _, value
-                          in age_layout.iterrows()]
+        self.rng.shuffle(age_cards) # Shuffle required when multiple card types are selected (i.e. 3rd Age).
+
+        initial_age_board = self.age_layouts[str(age)] # Selects appropriate list of CardSlot objects for age.
+
+        if len(initial_age_board) != len(age_cards):
+            return print('Number of card slots in chosen age does not match the number of cards selected!')
 
         # Place card objects into card slots
-        for slot, _ in enumerate(card_positions):
-            card_positions[slot].card_in_slot = Card(**age_cards.loc[slot])
+        for position, card in enumerate(age_cards):
+            initial_age_board[position].card_in_slot = card
 
-        # for slot in range(len(card_positions)):
-        #     card_positions[slot].card_in_slot = Card(**age_cards.loc[slot])
+        return initial_age_board
 
-        return card_positions
-
-    def update_all(self):
+    def update_all_slots(self):
         '''Updates all slots on board as per update_slot method'''
         for slot in range(len(self.card_positions)):
             self.update_slot(slot)  # Update each slot for visibility and selectability.
@@ -344,12 +372,25 @@ class Age:
     def display_board(self):
         '''Prints visual representation of cards remaining on the board for this age'''
         cards = self.card_positions
-        rows = self.number_of_rows
-        for row in reversed(range(rows + 1)):
-            print("Row", str(row + 1), ":", [card for card in cards if card.row == row])
+        rows = max(self.card_positions[slot].row for slot in range(len(self.card_positions)))
+        for row in reversed(range(int(rows) + 1)):
+            print("Row", str(row + 1), ":", [card for card in cards if int(card.row) == row])
+
+
+# Takes 2 Player objects and 1 Card object and checks whether card is constructable given state and cost.
+# TODO Check whether card is constructable given arbitrary player/opponent/card objects
+def card_constructable(player, opponent, card):
+    '''Checks whether a card is constructable given current player states'''
+    return True
+
+# Takes 2 Player objects and 1 Age object and retruns all valid moves for the player.
+# TODO Return list of valid moves for current player.
+def valid_moves(player, opponent, age):
+    '''Returns list of valid moves for given board state and player states'''
+    return
 
 
 if __name__ == "__main__":
-    game1 = Game(1)
+    game1 = Game(1,1)
+    game1.request_player_input()
     pass
-    # game1.request_player_input()
